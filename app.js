d7e048b24be21a471b14cf6075b9e742455222d5
PaataSet <paataseturidze@gmail.com>
2024-11-22 00:37:56 +0400

    finished homework

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ do {
   console.log(i);
   i++;
 } while (i < 30);
+
+ან 2-ე დავალება უნდა ჩანდეს ან 3-ე, ორივეს ერთად i-ის შემთხვევაში აერორებს ბრაუზერში.
+ამიტომ გამოვიყენე "j"
 */
 
 let j = 0;
@@ -35,7 +38,46 @@ do {
 
 const numbers = [2, 3, 6, 7, 8, 9, 23, 24, 29];
 
+let even = [];
+
+for (let index = 0; index < numbers.length; index++) {
+  if (numbers[index] % 2 == 0) even.push(numbers[index]);
+}
+
+console.log(even);
+
 // 5. გამოიყენეთ const currentDay = new Date().getDay(); ცვლადი;   currentDay ცვლადი გვაძლევს რეალურ კვირის დღეს
 //რიცხვითი სახით 0 - არის კვირა, 1 - არის ორშაბათი და ასე შემდეგ.( 0 - 6 ამდე რიცხვს აბრუნებს სადაც 6 არის შაბათი,
 //ყოველდღე სხვადასხვა მნიშვნელობას დაგიბრუნებთ) , დავწეროთ switch სადაც შევამოწმებთ currentDay-ს და 0 ის
 //შემთხვევაში console.log ში გამოვიტანთ სიტყვას - კვირა, 1 ის შემთხვევაში - ორშაბათს და ასე შემდეგ 6 - შაბათი.
+
+const currentDay = new Date().getDay();
+
+// console.log(currentDay);
+
+switch (currentDay) {
+  case 0:
+    console.log("კვირა");
+    break;
+  case 1:
+    console.log("ორშაბათი");
+    break;
+  case 2:
+    console.log("სამშაბათი");
+    break;
+  case 3:
+    console.log("ოთხშაბათი");
+    break;
+  case 4:
+    console.log("ხუთშაბათი");
+    break;
+  case 5:
+    console.log("პარასკევი");
+    break;
+  case 6:
+    console.log("შაბათი");
+    break;
+
+  default:
+    break;
+}
